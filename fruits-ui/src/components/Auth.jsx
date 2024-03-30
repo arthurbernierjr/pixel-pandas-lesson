4c97c56f701f98905215b2a9a329e8a1dcf593e8
Arthur Bernier Jr <ma2bernier@gmail.com>
2024-03-30 16:15:12 -0400

    Changes Noted Below

--- fruits-ui/src/components/Auth.jsx
+++ fruits-ui/src/components/Auth.jsx
@@ -3,6 +3,43 @@ import {useState} from 'react'
 
 export default function Auth(props){
     const [showLogin, setShowLogin] = useState(true)
+    const [formData, setFormdata] = useState({
+        username: '',
+        password: ''
+    })
+    const handleLogin = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            const data = await response.json()
+            props.setUser(data.user)
+            localStorage.setItem('token', data.token)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleSignUp = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            const data = await response.json()
+            props.setUser(data.newUser)
+            localStorage.setItem('token', data.token)
+        } catch (error) {
+            console.error(error)
+        }
+    }
     return(
         <div>
             {
