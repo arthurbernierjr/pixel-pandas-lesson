23a03dc857cb308fc7849bf56127d3d3d579ba43
Arthur Bernier Jr <ma2bernier@gmail.com>
2024-03-30 12:50:02 -0400

    Changes Noted Below

--- fruitsAPI/models/user.js
+++ fruitsAPI/models/user.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    // fruits: [{ type: mongoose.Types.ObjectId, ref: 'Fruit'}]
+    fruits: [{ type: mongoose.Types.ObjectId, ref: 'Fruit'}]
+}, {
+    timestamps: true, 
+    toJSON: {
+        transform: function(doc, ret){
+            delete ret.password
+            return ret
+        }
+    }
 })
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 6)
+    return next()
+})
+
 
 module.exports = mongoose.model('User', userSchema)
